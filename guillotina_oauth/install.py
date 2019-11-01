bff7cfeab4dddff9e5ba150bcf00ab2029f1692e
Nathan Van Gheem <vangheem@gmail.com>
2019-11-01 09:30:43 -0400

    Retry 503s (#17)

--- guillotina_oauth/install.py
+++ guillotina_oauth/install.py
@@ -4,24 +4,17 @@ from guillotina.addons import Addon
 from guillotina.interfaces import ILayers
 
 
-POAUTH_LAYER = 'guillotina_oauth.interfaces.IPOAuthLayer'
+POAUTH_LAYER = "guillotina_oauth.interfaces.IPOAuthLayer"
 
 
-@configure.addon(
-    name="poauth",
-    title="Plone OAuth Login")
+@configure.addon(name="poauth", title="Plone OAuth Login")
 class POauthAddon(Addon):
-
     @classmethod
     def install(cls, site, request):
         registry = request.site_settings
-        registry.forInterface(ILayers).active_layers |= {
-            POAUTH_LAYER
-        }
+        registry.forInterface(ILayers).active_layers |= {POAUTH_LAYER}
 
     @classmethod
     def uninstall(cls, site, request):
         registry = request.site_settings
-        registry.forInterface(ILayers).active_layers -= {
-            POAUTH_LAYER
-        }
+        registry.forInterface(ILayers).active_layers -= {POAUTH_LAYER}
