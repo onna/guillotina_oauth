bf236d39f33ee77021b6a9a6fe2026e9ff9969d1
Ferran Llamas <llamas.arroniz@gmail.com>
2020-05-11 10:53:34 +0200

    fix

--- guillotina_oauth/tests/test_setup.py
+++ guillotina_oauth/tests/test_setup.py
@@ -2,7 +2,10 @@
 from guillotina.component import getUtility
 
 from guillotina_oauth.oauth import IOAuth
+import pytest
+
+pytestmark = pytest.mark.asyncio
 
 
-def test_auth_registered(dummy_guillotina):
+async def test_auth_registered(dummy_guillotina):
     assert getUtility(IOAuth) is not None
